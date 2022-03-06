51d1a733ec6fc36845b961f349822d5a81b7b757
gogins <michael.gogins@gmail.com>
2022-03-06 17:37:43 +0100

    Fixed macOS build script.

--- build-cxx-macos.sh
+++ build-cxx-macos.sh
@@ -1,22 +1,6 @@
 #!/bin/bash
-echo "This build script assumes the use of llvm@13 installed with homebrew."
 rm clang_opcodes.dylib
-# To use the bundled libc++ please add the following LDFLAGS:
-# LDFLAGS="-L/opt/homebrew/opt/llvm/lib -Wl,-rpath,/opt/homebrew/opt/llvm/lib"
-
-#llvm is keg-only, which means it was not symlinked into /opt/homebrew,
-#because macOS already provides this software and installing another version in
-#parallel can cause all kinds of trouble.
-
-#If you need to have llvm first in your PATH, run:
-#  echo 'export PATH="/opt/homebrew/opt/llvm/bin:$PATH"' >> ~/.zshrc
-export PATH="/opt/homebrew/cellar/llvm/13.0.1/bin/:$PATH"
-
-# For compilers to find llvm you may need to set:
-export LDFLAGS="-L/opt/homebrew/cellar/llvm/13.0.1/lib -Wl,-rpath,opt/homebrew/cellar/llvm/13.0.1/lib"
-export CPPFLAGS="-I/opt/homebrew/cellar/llvm/13.0.1/include"
-export CLANGLIBS2="-lclangTooling -lclangFrontendTool -lclangFrontend -lclangDriver -lclangSerialization -lclangCodeGen -lclangParse -lclangSema -lclangStaticAnalyzerFrontend -lclangStaticAnalyzerCheckers -lclangStaticAnalyzerCore -lclangAnalysis -lclangARCMigrate -lclangRewrite -lclangRewriteFrontend -lclangEdit -lclangAST -lclangASTMatchers -lclangLex -lclangBasic -lclang"
 which clang++
-clang++ -v -g -O2 -fPIC -shared `/opt/homebrew/cellar/llvm/13.0.1/bin/llvm-config --cxxflags --ldflags` -I/usr/local/include -I/usr/local/include/csound -I/Library/Frameworks/CsoundLib64.framework/Headers cxx_opcodes.cpp ${CLANGLIBS2} `/opt/homebrew/cellar/llvm/13.0.1/bin/llvm-config --libs --system-libs` -o cxx_opcodes.dylib
+clang++ -v -g -O2 -fPIC -shared -I/usr/local/include -I/usr/local/include/csound -I/Library/Frameworks/CsoundLib64.framework/Headers cxx_opcodes.cpp -o cxx_opcodes.dylib
 ls -ll
 csound --opcode-lib="./cxx_opcodes.dylib" cxx_hello.csd
