dc23ba683fbcbd03eb74dc64c02762a22a72a041
gogins <michael.gogins@gmail.com>
2022-03-18 12:54:47 +0100

    Tweaking code (#17).

--- build-cxx-macos.sh
+++
@@ -1,6 +0,0 @@
-#!/bin/bash
-rm clang_opcodes.dylib
-which clang++
-clang++ -v -g -O2 -fPIC -shared -I/usr/local/include -I/usr/local/include/csound -I/Library/Frameworks/CsoundLib64.framework/Headers cxx_opcodes.cpp -o cxx_opcodes.dylib
-ls -ll
-csound --opcode-lib="./cxx_opcodes.dylib" cxx_hello.csd
